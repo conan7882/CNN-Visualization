32497db1259ac4cf600e35c7f3cf5820ef71f6f5
Qian Ge <qge2@ncsu.edu>
2017-10-11 00:32:51 -0400

    correct coding style

--- README.md
+++ README.md
@@ -1,14 +1,24 @@
 # Visualization of Deep Covolutional Neural Networks
 
 
-This repository contains implementations of visualizatin of CNN in recent papers:
+- This repository contains implementations of visualizatin of CNN in recent papers.
 
-- [Class Activation Mapping (CAM)](https://github.com/conan7882/CNN-Visualization/tree/master/class_activation_map)
+- The source code in the repository can be used to demostrate the algorithms as well as test on your own data.
+
+## Requirements
+- Python 3.3+
+- [Tensorflow 1.3](https://www.tensorflow.org/)
+- [TensorCV](https://github.com/conan7882/DeepVision-tensorflow) 
+
+
+## Algorithms 
+
+- [Class Activation Mapping (CAM)](https://github.com/conan7882/CNN-Visualization/tree/master/class_activation_map) (CVPR 16)
 
 ## Class Activation Mapping (CAM)
 - The class activation map highlights the most informative image regions relevant to the predicted class. This map can be obtained by adding a global average pooling layer at the end of convolutional layers.
 
-- Details of the implementation and more results are [here](https://github.com/conan7882/CNN-Visualization/tree/master/class_activation_map). Some results:
+- Details of the implementation and more results can be find [here](https://github.com/conan7882/CNN-Visualization/tree/master/class_activation_map). Some results:
 
 ![celtech_change](class_activation_map/figs/celtech_diff.png)
 
--- class_activation_map/vgg.py
+++ class_activation_map/vgg.py
@@ -19,10 +19,10 @@ NUM_CHANNEL = 3
 
 def get_config(FLAGS):
     # data for training
-    dataset_train = ImageLabelFromFolder(FLAGS.type, data_dir = config_path.data_dir, 
-                        num_class = FLAGS.nclass,
-                        resize = 224,
-                        num_channel = NUM_CHANNEL)
+    dataset_train = ImageLabelFromFolder(FLAGS.type, data_dir=config_path.data_dir, 
+                        num_class=FLAGS.nclass,
+                        resize=224,
+                        num_channel=NUM_CHANNEL)
 
     # Print image class name and label
     # print(dataset_train.label_dict)
@@ -30,58 +30,58 @@ def get_config(FLAGS):
     # Since the aim of training is visulization of class map, all the images 
     # are used for training. Using the training set as validation set is just
     # for checking whether the training works correctly.
-    dataset_val = ImageLabelFromFolder(FLAGS.type, data_dir = config_path.data_dir, 
-                        num_class = FLAGS.nclass,
-                        resize = 224,
-                        num_channel = NUM_CHANNEL)
+    dataset_val = ImageLabelFromFolder(FLAGS.type, data_dir=config_path.data_dir, 
+                        num_class=FLAGS.nclass,
+                        resize=224,
+                        num_channel=NUM_CHANNEL)
 
     # Image use for inference the class acitivation map during training
     dataset_test = ImageFromFile(FLAGS.type, 
-                                data_dir = config_path.infer_data_dir, 
-                                shuffle = False,
-                                resize = 224,
-                                num_channel = NUM_CHANNEL)
+                                data_dir=config_path.infer_data_dir, 
+                                shuffle=False,
+                                resize=224,
+                                num_channel=NUM_CHANNEL)
 
     # Check accuracy during training using training set
     inference_list_validation = [InferScalars('accuracy/result', 'test_accuracy')]
     # Check class acitivation map during training
     inference_list_test = [
-           InferOverlay(['classmap/result', 'image'], ['map', 'image'], color = True),
-           InferImages('classmap/result', 'map', color = True)
+           InferOverlay(['classmap/result', 'image'], ['map', 'image'], color=True),
+           InferImages('classmap/result', 'map', color=True)
         ]
 
     return TrainConfig(
                  dataflow = dataset_train, 
-                 model = VGGCAM(num_class = NUM_CHANNEL, 
-                           inspect_class = FLAGS.label,
-                           learning_rate = 0.001,
-                           is_load = True,
-                           pre_train_path = config_path.vgg_dir),
+                 model = VGGCAM(num_class=NUM_CHANNEL, 
+                           inspect_class=FLAGS.label,
+                           learning_rate=0.001,
+                           is_load=True,
+                           pre_train_path=config_path.vgg_dir),
                  monitors = TFSummaryWriter(),
                  callbacks = [
-                    ModelSaver(periodic = 100),
-                    TrainSummary(key = 'train', periodic = 50),
+                    ModelSaver(periodic=100),
+                    TrainSummary(key='train', periodic=50),
                     FeedInferenceBatch(dataset_val, 
-                                  periodic = 100, 
-                                  batch_count = 10, 
-                                  inferencers = inference_list_validation),
-                    FeedInference(dataset_test, periodic = 50,
-                                  infer_batch_size = 4, 
-                                  inferencers = inference_list_test),
+                                  periodic=100, 
+                                  batch_count=10, 
+                                  inferencers=inference_list_validation),
+                    FeedInference(dataset_test, periodic=50,
+                                  infer_batch_size=4, 
+                                  inferencers=inference_list_test),
                     CheckScalar(['accuracy/result','loss/result'], 
-                                 periodic = 10),
+                                 periodic=10),
                   ],
-                 batch_size = FLAGS.bsize, 
-                 max_epoch = 25,
-                 summary_periodic = 50,
-                 default_dirs = config_path)
+                 batch_size=FLAGS.bsize, 
+                 max_epoch=25,
+                 summary_periodic=50,
+                 default_dirs=config_path)
 
 def get_predict_config(FLAGS):
     dataset_test = ImageFromFile(FLAGS.type, 
-                                data_dir = config_path.test_data_dir, 
-                                shuffle = False,
-                                resize = 224,
-                                num_channel = NUM_CHANNEL)
+                                data_dir=config_path.test_data_dir, 
+                                shuffle=False,
+                                resize=224,
+                                num_channel=NUM_CHANNEL)
     # dataset_test = ImageLabelFromFolder('.jpg', data_dir = config_path.data_dir, 
     #                     num_class = FLAGS.nclass,
     #                     resize = 224,
@@ -90,40 +90,40 @@ def get_predict_config(FLAGS):
              # PredictionScalar(['pre_label'], ['label']),
              # PredictionMeanScalar('accuracy/result', 'test_accuracy'),
              PredictionMat('classmap/result', ['test']),
-             PredictionOverlay(['classmap/result', 'image'], ['map', 'image'], color = True, merge_im = True),
-             PredictionImage(['image'], ['image'], color = True, merge_im = True)
+             PredictionOverlay(['classmap/result', 'image'], ['map', 'image'], color=True, merge_im=True),
+             PredictionImage(['image'], ['image'], color=True, merge_im=True)
              ]
 
     return PridectConfig(
                 dataflow = dataset_test,
-                model = VGGCAM(num_class = NUM_CHANNEL, 
-                           inspect_class = FLAGS.label,
-                           learning_rate = 0.001,
-                           is_load = True,
-                           pre_train_path = config_path.vgg_dir),
-                model_name = FLAGS.model,
-                predictions = prediction_list,
-                batch_size = FLAGS.bsize,
-                default_dirs = config_path)
+                model = VGGCAM(num_class=NUM_CHANNEL, 
+                           inspect_class=FLAGS.label,
+                           learning_rate=0.001,
+                           is_load=True,
+                           pre_train_path=config_path.vgg_dir),
+                model_name=FLAGS.model,
+                predictions=prediction_list,
+                batch_size=FLAGS.bsize,
+                default_dirs=config_path)
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--bsize', default = 32, type = int)
-    parser.add_argument('--label', default = 1, type = int,
-                        help = 'Label of inspect class.')
-    parser.add_argument('--nclass', default = 257, type = int, 
-                        help = 'number of image class')
+    parser.add_argument('--bsize', default=32, type=int)
+    parser.add_argument('--label', default=1, type=int,
+                        help='Label of inspect class.')
+    parser.add_argument('--nclass', default=257, type=int, 
+                        help='number of image class')
 
-    parser.add_argument('--predict', action = 'store_true', 
-                        help = 'Run prediction')
-    parser.add_argument('--train', action = 'store_true', 
-                        help = 'Train the model')
+    parser.add_argument('--predict', action='store_true', 
+                        help='Run prediction')
+    parser.add_argument('--train', action='store_true', 
+                        help='Train the model')
 
-    parser.add_argument('--type', default = '.jpg', type = str, 
-                        help = 'image type for training and testing')
+    parser.add_argument('--type', default='.jpg', type=str, 
+                        help='image type for training and testing')
 
-    parser.add_argument('--model', type = str, 
-                        help = 'file name of the trained model')
+    parser.add_argument('--model', type=str, 
+                        help='file name of the trained model')
 
     return parser.parse_args()
 
