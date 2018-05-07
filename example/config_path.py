1c3bf772b2a92194b9a899f75b793ecd2e06a4f6
Qian Ge <qge2@ncsu.edu>
2018-05-07 11:27:04 -0500

    clean

---
+++ example/config_path.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File: config_path.py
+# Author: Qian Ge <geqian1001@gmail.com>
+
+# foler for saving
+save_path = '/Users/gq/tmp/viz/'
+
+# directory of pre-trained googlenet parameters
+googlenet_path = '/Users/gq/workspace/Dataset/pretrained/googlenet.npy'
--- example/vizfilter.py
+++ example/vizfilter.py
@@ -4,27 +4,26 @@
 # Author: Qian Ge <geqian1001@gmail.com>
 
 import os
-import argparse
-import numpy as np
 import tensorflow as tf
+
+import config_path as config
 
 import sys
 sys.path.append('../')
 from lib.nets.googlenet import BaseGoogLeNet
 import lib.utils.viz as viz
 
-pre_train_path = '/Users/gq/workspace/Dataset/pretrained/googlenet.npy'
-save_path = '/Users/gq/tmp/viz/'
 
 if __name__ == '__main__':
 
-    model = BaseGoogLeNet(pre_train_path)
-    filters = tf.get_default_graph().get_tensor_by_name('conv1_7x7_s2/weights:0')
+    model = BaseGoogLeNet(config.googlenet_path)
+    filters = tf.get_default_graph().get_tensor_by_name(
+        'conv1_7x7_s2/weights:0')
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         learned_filter = sess.run(filters)
         viz.viz_filters(learned_filter,
                         [8, 8],
-                        os.path.join(save_path, 'test.png'),
+                        os.path.join(config.save_path, 'GoogLeNet.png'),
                         gap=2)
