a6be71b7bacb575ce59e00e776c677351b7605dd
Qian Ge <qge2@ncsu.edu>
2018-09-12 09:07:44 -0400

    Update config_path.py

--- example/config_path.py
+++ example/config_path.py
@@ -5,14 +5,11 @@
 
 # directory of testing images
 # im_path = '../data/' 
-im_path = '/home/qge2/workspace/data/dataset/256_ObjectCategories/159.people/'
+im_path = '../data/dataset/256_ObjectCategories/159.people/'
 
 # foler for saving
-# save_path = '/Users/gq/tmp/viz/'
 save_path = ''
 
 # directory of pre-trained googlenet parameters
-googlenet_path = '/Users/gq/workspace/Dataset/pretrained/googlenet.npy'
-# vgg_path = 'E://GITHUB//workspace//CNN//pretrained//vgg19.npy'
-# vgg_path = '/Users/gq/workspace/Dataset/pretrained/vgg19.npy'
-vgg_path = '/home/qge2/workspace/data/pretrain/vgg/vgg19.npy'
+googlenet_path = '../pretrained/googlenet.npy'
+vgg_path = '../pretrain/vgg/vgg19.npy'
